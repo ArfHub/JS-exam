4a597ec70cf2d741568bc081e6fe91ed6c779762
ARFHSI <Abdulraouf.hsi@gmail.com>
2024-06-05 20:22:00 +0300

    js-exam

---
+++ main.js
@@ -0,0 +1,18 @@
+
+function date(){
+    document.getElementById("date").innerHTML = "Date: 01-09-1439"
+}
+function date2(){
+    document.getElementById("date").innerHTML = "Date: 15-05-2018"
+}
+
+
+function validatePass(){
+ let name = document.getElementById('username').value;
+ name.value 
+
+}
+
+
+
+
